fc07c13452b70c78037ab2d4ff05557ffee6c6a6
Florian Solbach <96florian@web.de>
2017-07-03 15:36:23 +0200

    added a few things to the Game Class

--- app/src/main/java/io/github/faflostuso/guesswhat/gamelogic/Game.java
+++ app/src/main/java/io/github/faflostuso/guesswhat/gamelogic/Game.java
@@ -1,5 +1,7 @@
 package io.github.faflostuso.guesswhat.gamelogic;
 
+import java.util.ArrayList;
+
 /**
  * Created by florian on 03.07.17.
  */
@@ -7,9 +9,29 @@ package io.github.faflostuso.guesswhat.gamelogic;
 public class Game {
     public final int POINTS_FOR_CORRECT_ANSWER;
     public final int POINTS_FOR_DECEIVING;
+    private ArrayList<Question> unusedQuestion; //TODO als Set!
+    private ArrayList<Player> players;
+    private boolean gameActive;
 
     public Game(int points_for_correct_answer, int points_for_deceiving) {
         POINTS_FOR_CORRECT_ANSWER = points_for_correct_answer;
         POINTS_FOR_DECEIVING = points_for_deceiving;
+    }
+
+    public void addPlayer(){
+        //TODO nur hinzufügen wenn Spiel noch nicht begonnen
+    }
+
+    public void addQuestion(Question question){
+        //TODO exception werfen, wenn Frage schon vorhanden
+        unusedQuestion.add(question);
+    }
+
+    public void startGame(){
+
+    }
+
+    public boolean isGameActive(){
+        return this.isGameActive();
     }
 }
